4017375dfeffc1e6c736b77b90ed4348306ca4f0
elloco912 <vlarosa9121992@gmail.com>
2020-07-11 11:35:13 -0500

    11072020

--- HistClinica/Clinica2.0/Controllers/LoginController.cs
+++ HistClinica/Clinica2.0/Controllers/LoginController.cs
@@ -1,39 +1,97 @@
-﻿using Clinica2._0.Data;
+﻿using Clinica2._0.DTO;
 using Clinica2._0.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Clinica2._0.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
-        private readonly UsuarioContext _context;
+        private readonly UserManager<USUARIO> _userManager;
+        private readonly SignInManager<USUARIO> _signInManager;
         private readonly ILogger<LoginController> _logger;
 
-        public LoginController(UsuarioContext context, ILogger<LoginController> logger)
+        public LoginController(SignInManager<USUARIO> signInManager,
+            ILogger<LoginController> logger,
+            UserManager<USUARIO> userManager)
         {
-            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
             _logger = logger;
         }
-        public IActionResult Index()
+
+        [BindProperty]
+        public Logueo Input { get; set; }
+
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
+            string returnUrl = null;
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            ReturnUrl = returnUrl;
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(USUARIO usu)
+        public async Task<IActionResult> Index(string returnUrl = null)
         {
-            var user = _context.USUARIO.Where(u => u.UserName.Equals(usu.UserName) 
-            //&& u.PasswordHash.Equals(usu.PasswordHash)
-            ).FirstOrDefault();
-            if (user != null)
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
             {
-               // HttpContext.Session.SetString("nombreusuario", "Bertol Franco");
-                return RedirectToAction("Index", "Panel");
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect("/Panel/Index");
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("User account locked out.");
+                    return RedirectToRoute("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View();
+                }
             }
-            ViewBag.error = "Usuario invalido";
+
+            // If we got this far, something failed, redisplay form
             return View();
         }
     }
