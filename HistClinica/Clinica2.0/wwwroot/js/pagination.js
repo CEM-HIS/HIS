e691ae1708248f3cfd8954a35ef4b8ff1d4135c5
nathly <nathalysalgado.f@gmail.com>
2020-07-31 14:32:01 -0500

    update pagination.js

--- HistClinica/Clinica2.0/wwwroot/js/pagination.js
+++ HistClinica/Clinica2.0/wwwroot/js/pagination.js
@@ -24,7 +24,7 @@
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
         },
-        "scrollY": "300px",
+        "scrollY": "600px",
         "scrollCollapse": true,
     });
     $('#licenciagrid').DataTable({
